1d9d1b67af59212cf6c3cbef219877f64b24f68a
Joris De Winne <joris.dewinne@gmail.com>
2018-06-28 20:59:18 -0700

    Using travis for automated builds

--- README.md
+++ README.md
@@ -5,5 +5,4 @@ Based on [Jenkins base image](https://hub.docker.com/_/jenkins/)
 
 ## Plugins ##
 + deployit-plugin:5.0.3 
-+ xltestview-plugin:1.2.0 
 + xlrelease-plugin:5.0.0
---
+++ travis-ci.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+image_name="xebialabsunsupported/xl-docker-demo-jenkins"
+./dockertags jenkins > /tmp/jenkins
+./dockertags $image_name > /tmp/xl
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+while read tag ; do
+    docker build -t $image_name:$tag --build-arg jenkins_tag=$tag .
+    docker push $image_name:$tag
+done < <(comm -23 <(sort /tmp/jenkins) <(sort /tmp/xl))
